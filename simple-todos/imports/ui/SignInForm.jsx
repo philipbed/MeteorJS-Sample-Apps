3a09b976fd13d6d46f06b34c72d9a77273b5c01f
philipbed <philipbed813@gmail.com>
2016-11-03 16:23:45 -0400

    Corrected unbalanced tags

--- simple-todos/imports/ui/SignInForm.jsx
+++ simple-todos/imports/ui/SignInForm.jsx
@@ -21,35 +21,36 @@ export default class SignInForm extends Component{
   render(){
     return (
       <div className="container">
-        <div className="row">
-            <div className="col-md-6 col-md-offset-3"></div>
-            <form className="signInForm" onSubmit={this.handleSubmit.bind(this)}>
-              <div className="form-group ">
-                <label htmlFor="email">Email:</label>
-                <input
-                  id="email"
-                  className="input-medium col-sm-offset-1"
-                  type="email"
-                  ref="username"
-                  placeholder="Username"
-                />
-              </div>
-              <div className="form-group">
-                <label>Password:</label>
-                <input
-                  className="input-medium"
-                  type="password"
-                  ref="password"
-                  placeholder="Password"
-                />
+          <div className="row">
+              <div className="col-md-6 col-md-offset-3">
+                <form className="signInForm" onSubmit={this.handleSubmit.bind(this)}>
+                  <div className="form-group ">
+                    <label htmlFor="email">Email:</label>
+                    <input
+                      id="email"
+                      className="input-medium col-sm-offset-1"
+                      type="email"
+                      ref="username"
+                      placeholder="Username"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label>Password:</label>
+                    <input
+                      className="input-medium"
+                      type="password"
+                      ref="password"
+                      placeholder="Password"
+                    />
 
+                  </div>
+                    <button type="submit" ref="regButton" className="btn">Login</button>
+                </form>
               </div>
-                <button type="submit" ref="regButton" className="btn">Login</button>
-            </form>
-
-        <h3>
-          Need to create and Account? <SignUpButton/>
-        </h3>
+          <h3>
+            Need to create and Account? <SignUpButton/>
+          </h3>
+        </div>
       </div>
     );
   }
